a4ab433aaa05b1282a3ef07b4212cf104ea2d946
Stephen Wright <scw248@gmail.com>
2019-02-02 21:48:39 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,19 +7,20 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    student_array = []
+    student_index_array = []
     
     doc.css(".roster-cards-container").each do |profile|
-      student = {}
-      #binding.pry
-      student[:name] = profile.css(".student-card a div h4.student-name").text
-      student[:location] = profile.css(".student-card a div p.student-location").text
-      student[:profile_url] = profile.css("div.student-card a").attribute("href").value
-      student_array << student
+      profile.css(".student-card a").each do |student_card|
+        student = {}
+        student[:name] = student_card.css(".student-name").text
+        student[:location] = student_card.css(".student-location").text
+        student[:profile_url] = student_card.attribute("href").value
+        student_index_array << student
+      end
+      
     end
-    binding.pry
-    student_array
-    
+    #binding.pry
+    student_index_array
   end
 
   def self.scrape_profile_page(profile_url)
@@ -27,16 +28,43 @@ class Scraper
     attributes = {}
     
     doc.css("html").each do |attribute|
-      attributes[:twitter] = attribute.css("div.social-icon-container a").attribute("href").value unless !attribute.css("div.social-icon-container a").attribute("href").value
-      attributes[:linkedin] = attribute.css("div.social-icon-container a").attribute("href").value unless !attribute.css("div.social-icon-container a").attribute("href").value
-      attributes[:github] = attribute.css("div.social-icon-container a").attribute("href").value unless !attribute.css("div.social-icon-container a").attribute("href").value
-      attributes[:blog] = attribute.css("div.social-icon-container a").attribute("href").value unless !attribute.css("div.social-icon-container a").attribute("href").value
-      attributes[:profile_quote] = attribute.css("div.profile-quote").text
-      attributes[:bio] = attribute.css("div.description-holder p").text
+      link_array = []
+      attribute.css("div.social-icon-container").children.css("a").each do |link|
+        
+        link_array << link.attribute("href").value
+        #binding.pry
+      end
+      
+      link_array.each do |link|
+        if link.include?("twitter")
+          attributes[:twitter] = link
+        elsif link.include?("linkedin")
+          attributes[:linkedin] = link
+        elsif link.include?("github")
+          attributes[:github] = link
+        elsif link.include?("blog")
+          attributes[:blog] = link
+        end
+      end
+      
+      attribute.css("div.description-holder p").text.each do |bio|
+        attributes[:bio] = bio
+      end
+      
+      attributes
+      
     end
+          
+          link_array
+      
+    #   attributes[:twitter] = attribute.css("div.social-icon-container a").attribute("href").value 
+    #   attributes[:linkedin] = attribute.css("div.social-icon-container a").attribute("href").value 
+    #   attributes[:github] = attribute.css("div.social-icon-container a").attribute("href").value 
+    #   attributes[:blog] = attribute.css("div.social-icon-container a").attribute("href").value 
+    #   attributes[:bio] = attribute.css("div.description-holder p").text
+    # end
     #binding.pry
     attributes
   end
-
 
 end
